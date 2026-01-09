26d178963229b48a7ea3c66a0e392d62fbf9d4e4
Gustavo Sacomani Rafael <gustavo.sr.dev@gmail.com>
2026-01-09 11:54:00 -0300

    Implement population growth calculation

--- CS50/C/population.c
+++ CS50/C/population.c
@@ -1,1 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    int start;
+    do
+    {
+        start = get_int("Start size: ");
+    }
+    while (start < 9);
+
+    int end;
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end < start);
+
+    int years = 0;
+    int population = start;
+
+    while (population < end)
+    {
+        population = population + (population / 3) - (population / 4);
+        years++;
+    }
+
+    printf("Years: %i\n", years);
+}
