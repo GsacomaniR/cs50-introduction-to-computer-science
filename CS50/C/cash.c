f9f7b7e6a0fba9e90cc6e8b78d7c6eb14fc04698
Gustavo Sacomani Rafael <g.sacomani.r@gmail.com>
2026-01-09 11:52:39 -0300

    Implement cash change calculation program

--- CS50/C/cash.c
+++ CS50/C/cash.c
@@ -1,1 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
 
+int main(void)
+{
+    float reais;
+    do
+    {
+        reais = get_float("Troca devida: ");
+    }
+    while (reais < 0);
+    int centavos = round(reais * 100);
+    int moedas = 0;
+    moedas += centavos / 25;
+    centavos = centavos % 25;
+    moedas += centavos / 10;
+    centavos = centavos % 10;
+    moedas += centavos / 5;
+    centavos = centavos % 5;
+    moedas += centavos;
+    printf("%i\n", moedas);
+}
+ 
