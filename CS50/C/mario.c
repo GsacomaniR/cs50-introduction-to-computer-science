406e4e107024c421fe227efc7b1e5f01d474b45c
Gustavo Sacomani Rafael <g.sacomani.r@gmail.com>
2026-01-09 11:53:24 -0300

    Add pyramid printing functionality in mario.c
    Implements a pyramid pattern based on user input height.

--- CS50/C/mario.c
+++ CS50/C/mario.c
@@ -1,1 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    int altura;
+
+    do
+    {
+        altura = get_int("Altura: ");
+    }
+    while (altura < 1 || altura > 8);
+
+    for (int linha = 1; linha <= altura; linha++)
+    {
+        // imprime espaços
+        for (int espaco = 0; espaco < altura - linha; espaco++)
+        {
+            printf(" ");
+        }
+
+        // imprime #
+        for (int hash = 0; hash < linha; hash++)
+        {
+            printf("#");
+        }
+
+        // pula linha
+            printf("  ");
+            for (int hash = 0; hash < linha; hash++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
