cbfb00ee679f77695a596a050b7d62d6af26adca
Gustavo Sacomani Rafael <gustavo.sr.dev@gmail.com>
2026-05-16 21:14:15 -0300

    Update dictionary.c

--- CS50/Data_Structure/speller/dictionary.c
+++ CS50/Data_Structure/speller/dictionary.c
@@ -2,6 +2,10 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
@@ -12,43 +16,120 @@ typedef struct node
     struct node *next;
 } node;
 
-// TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+// Number of buckets in hash table (ajuste para melhor performance)
+// 100000 é um bom número para 143091 palavras (fator de carga ~1.43)
+const unsigned int N = 100000;
 
 // Hash table
 node *table[N];
 
+// Word counter for size function
+unsigned int word_count = 0;
+
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    // Get hash value
+    unsigned int index = hash(word);
+
+    // Traverse linked list at this index
+    node *cursor = table[index];
+    while (cursor != NULL)
+    {
+        // Compare case-insensitively
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
     return false;
 }
 
-// Hashes word to a number
+// Hashes word to a number (djb2 hash function by Dan Bernstein)
+// Source: http://www.cse.yorku.ca/~oz/hash.html
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    unsigned long hash = 5381;
+    int c;
+
+    // Convert to lowercase while hashing for case-insensitivity
+    while ((c = tolower(*word++)))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return hash % N;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // Initialize hash table
+    for (int i = 0; i < N; i++)
+    {
+        table[i] = NULL;
+    }
+
+    // Open dictionary file
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+
+    // Buffer for word
+    char word[LENGTH + 1];
+
+    // Read each word from file
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        // Create new node
+        node *new_node = malloc(sizeof(node));
+        if (new_node == NULL)
+        {
+            fclose(file);
+            return false;
+        }
+
+        // Copy word into node
+        strcpy(new_node->word, word);
+        new_node->next = NULL;
+
+        // Get hash value
+        unsigned int index = hash(word);
+
+        // Insert node into hash table (at beginning)
+        new_node->next = table[index];
+        table[index] = new_node;
+
+        // Increment word counter
+        word_count++;
+    }
+
+    // Close dictionary file
+    fclose(file);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    // Free all nodes in hash table
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+        }
+    }
+    return true;
 }
